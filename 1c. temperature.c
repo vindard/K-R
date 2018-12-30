9a5693108efb3e5b28df9730ed71e40eed525f31
vindard <vinda.rd@gmail.com>
2018-12-29 21:52:29 -0400

    refactor temperature.c to use...
    symbolic constants and a for loop

--- 1c. temperature.c
+++ 1c. temperature.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0    // lower limit of table
+#define UPPER 300    // upper limit
+#define STEP 20    //step size
+
 int main(void)
 {
-    float fahr, celsius;
-    float lower, upper, step;
-
-    lower = 0; /* lower limit of temperatuire scale */
-    upper = 300; /* upper limit */
-    step = 20; /* step size */
-
-    printf("  F      C\n");
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+    int fahr;
+    
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
     return 0;
 }
