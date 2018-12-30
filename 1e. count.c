8262faf4cdb02838cfc9be5a97fe8bbadba49e7b
vindard <vinda.rd@gmail.com>
2018-12-29 22:27:28 -0400

    add count.c

---
+++ 1e. count.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* count characters in input; 1st version */
+int main()
+{
+    long nc;
+
+    nc = 0;
+
+    while (getchar() != '~') {
+        ++nc;
+        printf("%ld\n", nc);
+    }
+
+    return 0;
+}
