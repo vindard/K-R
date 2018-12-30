a856dae9a9ea3aa97bcdc2b0adbe6e03bf1cd797
vindard <vinda.rd@gmail.com>
2018-12-29 21:58:18 -0400

    add eof.c

---
+++ 1d. eof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* copy input to putput; 1st version */
+int main()
+{
+    int c;
+
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+    return 0;
+}
